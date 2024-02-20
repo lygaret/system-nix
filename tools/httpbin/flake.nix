2533c57500b36ed30f1280d00cb16d0598ea9805
Jonathan Raphaelson <jonathan@ada-marie.com>
2024-02-20 14:37:51 -0700

    consistent naming for imports

--- tools/httpbin/flake.nix
+++ tools/httpbin/flake.nix
@@ -2,11 +2,11 @@
   description = "httpbin";
 
   inputs = {
-    nixpkgs.url    = "github:nixos/nixpkgs/nixos-unstable";
-    flakeutils.url = "github:numtide/flake-utils";
+    nixpkgs.url     = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flakeutils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     # flakeutils.lib.eachDefaultSystem (system:
     let system = "aarch64-darwin"; in
       let
